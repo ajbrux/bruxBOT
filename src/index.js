145ee34a8386994df9d9b8ae7475643e3badb004
AJ <atticusglasswerks@gmail.com>
2025-10-05 04:19:14 -0700

    index.js vars, client, listener, print

--- src/index.js
+++ src/index.js
@@ -1,16 +1,45 @@
 // src/index.js
-imports
+import 'dotenv/config';
+import tmi from 'tmi.js';
 
 //read config from .env
+const username = process.env.TWITCH_BOT_USERNAME;
+const token    = process.env.TWITCH_OAUTH_TOKEN;
+const channel  = process.env.TWITCH_CHANNEL;
 
+if (!username || !token || channel) {
+    //kick error
+}
 
 //create client
+const client = new tmi.Client({
+    identity: { username, password: token },
+    channels:[channels]
+});
 
 
 //listen for chat
+client.on('message', async (_chan, tags, message, self) => {
+    if (self) return;
+
+
+//!sound alerts
+
+
+//!emoji alerts
+
+
+
+//listen for raid
+
+
+
+//raid alert
+
 
 
 //print to terminal
-
+  const name = tags['display-name'] || tags.username || 'unknown';
+  console.log(`${name}: ${message}`);
 
 //parser
