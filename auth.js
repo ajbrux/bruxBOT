02b9c615aca332889ab7b5f498c8787b275c9810
AJ <atticusglasswerks@gmail.com>
2025-10-05 22:15:12 -0700

    auth.js //NONOP variabled cleaned up, formatting, /callback const tokenRes fixed, local host token page/launch cleaned up,

--- auth.js
+++ auth.js
@@ -21,34 +21,35 @@ if (!CLIENT_ID || !CLIENT_SECRET) {
 
 }
 
+const TWITCH_AUTHORIZE_URL = 'https://id.twitch.tv/oauth2/authorize';
+const TWITCH_TOKEN_URL = 'https://id.twitch.tv/oauth2/token';
+
 //login and show Twitch authorize link
 app.get('/login', (_req, res) => {
-    const u = new URL('https://id.twitch.tv/oauth2/authorize')
+    const u = new URL(TWITCH_AUTHORIZE_URL);
     u.searchParams.set('client_id', CLIENT_ID);
     u.searchParams.set('redirect_uri', REDIRECT_URI);
     u.searchParams.set('response_type', 'code');
     u.searchParams.set('scope', );
     res.send(' <a href="${u.toString()}"> click to authorize </a> ')
+    u.searchParams.set('scope', SCOPES.join(' '));
 });
 
-//callback exchanges
-app.get(
-    '/callback', async (req, res) => {
-    const code = req.query.code;
-
-    if (!code)
-        return res.status(400).send('token exchange failed');
+// callback: exchange ?code for tokens
+app.get('/callback', async (req, res) => {
+    const code = req.query.code?.toString();
+    if (!code) return res.status(400).send('Missing ?code');
 
     try {
-    const tokenRes = await fetch({
-        method:     'POST',
-        headers:    {'Content-Type': 'application/x-www-form-urlencoded'},
-        body:       new URLSearchParams ({
-                        client_id: CLIENT_ID,
-                        client_secret: CLIENT_SECRET, code,
-                        grant_type: 'authorization_code',
-                        redirect_uri: REDIRECT_URI
-                    })
+        const tokenRes = await fetch(TWITCH_TOKEN_URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: new URLSearchParams({
+            client_id: CLIENT_ID,
+            client_secret: CLIENT_SECRET, code,
+            grant_type: 'authorization_code',
+            redirect_uri: REDIRECT_URI
+      })
     });
 
     //!tokens => kick error
@@ -67,8 +68,9 @@ app.get(
 
     //token to local host
     res.send(`
-        <p>${data.access_token}</p>
-        <p>${data.refresh_token}</p>
+      <h2>Success</h2>
+      <pre>TWITCH_OAUTH_TOKEN=oauth:${data.access_token}</pre>
+      <pre>REFRESH_TOKEN=${data.refresh_token}</pre>
     `);
 
     } catch (e) {
@@ -77,15 +79,9 @@ app.get(
     }
 });
 
-
-
-
-//start local server, open browser
-app.listen(
-    port, async () => {
-        try {await open('http://localhost:${port}/login');
-        } catch {}
-    }
-);
-
-
+// start server & open browser
+app.listen(port, async () => {
+    console.log(`Auth server running at http://localhost:${port}`);
+    console.log(`Open http://localhost:${port}/login to start`);
+        try { await open(`http://localhost:${port}/login`); } catch {}
+});
